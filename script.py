f85df70c84c1cc90d2be5957d40d37d05e5c3d70
unknown <haris.taraholic@outlook.com>
2024-10-22 20:36:39 +0200

    Planing how to create game in terminal

--- script.py
+++ script.py
@@ -1,15 +1,12 @@
 class Battlefield():
     #This class will descide how will battlefield look like by asking Players 1 and 2 where there ships are gooing to go.
     #The battlefield is gooing to be 5x5 for quicker play time.
+    #This class should also determen who won
     pass
 
-class PLayer_1():
-    #This class will be for player 1 and he has to first input name then choose where 
-    #ships are gooing to be on a battlefield in this class there should be plans to attack squares of player 2
-    #and wether he hit or miss oposing players ship.
-    pass
-
-class Player_2():
+class PLayers():
+    def __init__(self, input_name):
+        self.name = input_name
     #This class will be for player 1 and he has to first input name then choose where 
     #ships are gooing to be on a battlefield in this class there should be plans to attack squares of player 2
     #and wether he hit or miss oposing players ship.
