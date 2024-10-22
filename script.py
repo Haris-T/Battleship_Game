7e45237dc9edaabe425ef2371fff1dae86d342ec
unknown <haris.taraholic@outlook.com>
2024-10-22 20:21:52 +0200

    Planing how this game should work

---
+++ script.py
@@ -0,0 +1,16 @@
+class Battlefield():
+    #This class will descide how will battlefield look like by asking Players 1 and 2 where there ships are gooing to go.
+    #The battlefield is gooing to be 5x5 for quicker play time.
+    pass
+
+class PLayer_1():
+    #This class will be for player 1 and he has to first input name then choose where 
+    #ships are gooing to be on a battlefield in this class there should be plans to attack squares of player 2
+    #and wether he hit or miss oposing players ship.
+    pass
+
+class Player_2():
+    #This class will be for player 1 and he has to first input name then choose where 
+    #ships are gooing to be on a battlefield in this class there should be plans to attack squares of player 2
+    #and wether he hit or miss oposing players ship.
+    pass
